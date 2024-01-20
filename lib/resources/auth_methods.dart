9abdf5ec85ae30a28f0c0f19b5cc91add71429d8
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-20 19:06:46 +0900

    プロフィール画像選択機能追加

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_flutter/resources/storage_methods.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -28,6 +29,9 @@ class AuthMethods {
         );
         print(cred.user!.uid);
 
+        String photoUrl = await StorageMethods()
+            .uploadImageToStorage('profilePics', file, false);
+
         // add user to our database
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'username': username,
@@ -36,6 +40,7 @@ class AuthMethods {
           'bio': bio,
           'followers': [],
           'following': [],
+          'photoUrl': photoUrl,
         });
         res = 'success';
       }

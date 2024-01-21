1e848055bc35f6d6674475a062f8f2a5c3d85863
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-21 18:52:34 +0900

    ユーザーがをグイン状態であるかどうか監視する機能追加追加、UserProvider追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/providers/user_provider.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/screens/login_screen.dart';
 import 'package:instagram_flutter/screens/signup_screen.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyAZ82cwRpR4FhNvoE8AVFJtfCbvzJ2tO_8",
-            appId: "1:828625792901:web:d6d6ba8b8f1e15157a1eec",
-            messagingSenderId: "828625792901",
-            projectId: "instagram-flutter-3254d",
-            storageBucket: "instagram-flutter-3254d.appspot.com",
-            ),
-            );
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAZ82cwRpR4FhNvoE8AVFJtfCbvzJ2tO_8",
+        appId: "1:828625792901:web:d6d6ba8b8f1e15157a1eec",
+        messagingSenderId: "828625792901",
+        projectId: "instagram-flutter-3254d",
+        storageBucket: "instagram-flutter-3254d.appspot.com",
+      ),
+    );
   } else {
     await Firebase.initializeApp();
   }
@@ -31,17 +34,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram Clone',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor,
-      ),
-      // home: const ResponsiveLayout(
-      //   mobileScreenLayout: MobileScreenLayout(),
-      //   webScreenLayout: WebScreenLayout(),
-      // ),
-      home: const SignupScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Instagram Clone',
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: mobileBackgroundColor,
+          ),
+          home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.hasData) {
+                  return const ResponsiveLayout(
+                    mobileScreenLayout: MobileScreenLayout(),
+                    webScreenLayout: WebScreenLayout(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      '${snapshot.error}',
+                    ),
+                  );
+                }
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                const Center(
+                  child: CircularProgressIndicator(
+                    color: primaryColor,
+                  ),
+                );
+              }
+
+              return const LoginScreen();
+            },
+          )),
     );
   }
 }

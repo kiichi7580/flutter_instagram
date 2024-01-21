81451af30596821c914b6b05ccc0f2ee71fd5596
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-21 19:02:18 +0900

    bottomNavigationBarで複数のページを遷移できるようにした

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,14 +1,91 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/global_variables.dart';
+import 'package:provider/provider.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  @override
   Widget build(BuildContext context) {
+    int _page = 0;
+    late PageController pageController = PageController();
+
+    @override
+    void dispose() {
+      super.dispose();
+      pageController = PageController();
+    }
+
+    void navigationTapped(int page) {
+      pageController.jumpToPage(page);
+    }
+
+    void onPageChanged(int page) {
+      setState(() {
+        _page = page;
+      });
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text('This is mobile.')
-      ),
-    );
+        body: PageView(
+          controller: pageController,
+          onPageChanged: onPageChanged,
+          // 横スライドでページ遷移できないようにする
+          physics: const NeverScrollableScrollPhysics(),
+          children: homeScreenItems,
+        ),
+        bottomNavigationBar: CupertinoTabBar(
+          backgroundColor: mobileBackgroundColor,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: _page == 0 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: _page == 1 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_circle,
+                color: _page == 2 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: _page == 3 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: _page == 4 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+          ],
+          onTap: navigationTapped,
+        ));
   }
 }

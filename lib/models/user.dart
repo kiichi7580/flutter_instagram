b91b94761f392ecd2e749017555f254d2d0b398a
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-21 18:56:41 +0900

    Userクラスのインスタンスに変換するメソッド追加

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
   final String username;
   final String email;
@@ -26,4 +28,18 @@ class User {
         'following': following,
         'photoUrl': photoUrl,
       };
+
+  static User formSnap(DocumentSnapshot snap) {
+    var snapshot = (snap.data() as Map<String, dynamic>);
+
+    return User(
+      username: snapshot['username'],
+      email: snapshot['email'],
+      uid: snapshot['uid'],
+      bio: snapshot['bio'],
+      followers: snapshot['followers'],
+      following: snapshot['following'],
+      photoUrl: snapshot['photoUrl'],
+    );
+  }
 }

040bfe270b5a4fe0e3983cfd21ed2ab6ba4c2081
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-30 23:55:47 +0900

    投稿一覧をリアルタイムで取得できるよにした

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/models/user.dart';
 import 'package:instagram_flutter/providers/user_provider.dart';
@@ -34,7 +35,31 @@ class _CommentsScreenState extends State<CommentsScreen> {
         centerTitle: false,
         backgroundColor: mobileBackgroundColor,
       ),
-      body: CommentCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('posts')
+            .doc(widget.snap['postId'])
+            .collection('comments')
+            .orderBy(
+              'datePublished',
+              descending: true,
+            )
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: (snapshot.data! as dynamic).docs.length,
+            itemBuilder: (context, index) => CommentCard(
+              snap: (snapshot.data! as dynamic).docs[index].data(),
+            ),
+          );
+        },
+      ),
       bottomNavigationBar: SafeArea(
         child: Container(
           height: kToolbarHeight,
@@ -76,7 +101,11 @@ class _CommentsScreenState extends State<CommentsScreen> {
                     user.uid,
                     _commentController.text,
                     widget.snap['postId'],
+                    widget.snap['likes'],
                   );
+                  setState(() {
+                    _commentController.text = '';
+                  });
                 },
                 child: Container(
                   padding: const EdgeInsets.symmetric(

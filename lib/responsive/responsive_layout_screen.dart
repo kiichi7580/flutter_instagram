47a6585aa6682b4358af4edd580adaab9f7aef07
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-30 18:20:26 +0900

    userprovider追加

--- lib/responsive/responsive_layout_screen.dart
+++ lib/responsive/responsive_layout_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_flutter/utils/dimensions.dart';
+import 'package:instagram_flutter/models/user.dart' as model;
+import 'package:instagram_flutter/providers/user_provider.dart';
+import 'package:instagram_flutter/utils/global_variables.dart';
+import 'package:provider/provider.dart';
 
-class ResponsiveLayout extends StatelessWidget {
+class ResponsiveLayout extends StatefulWidget {
   const ResponsiveLayout({
     Key? key,
     required this.webScreenLayout,
@@ -12,14 +15,31 @@ class ResponsiveLayout extends StatelessWidget {
   final Widget mobileScreenLayout;
 
   @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+  @override
+  void initState() {
+    super.initState();
+    addData();
+  }
+
+  addData() async {
+    UserProvider _userProvider = Provider.of(context, listen: false);
+    await _userProvider.refreshUser();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    
     return LayoutBuilder(builder: (context, constraints) {
       if (constraints.maxWidth > webScreenSize) {
         // webスクリーン
-        return webScreenLayout;
+        return widget.webScreenLayout;
       }
       // mobileスクリーン
-      return mobileScreenLayout;
+      return widget.mobileScreenLayout;
     });
   }
 }

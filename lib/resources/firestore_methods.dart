48c0cf19efbbd78b70f3a7e85834f452b1419c6e
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-30 18:16:15 +0900

    firestoreに投稿を保存する機能と、いいね機能を更新する機能と、投稿のコメントを保存する機能を追加

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,92 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_flutter/models/post.dart';
+import 'package:instagram_flutter/resources/storage_methods.dart';
+import 'package:uuid/uuid.dart';
+
+class FirestoreMethods {
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+
+  Future<String> uploadPost(
+    String description,
+    Uint8List file,
+    String uid,
+    String username,
+    String profImage,
+  ) async {
+    String res = 'Some error occurred.';
+
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('posts', file, true);
+
+      String postId = const Uuid().v1();
+
+      Post post = Post(
+        description: description,
+        uid: uid,
+        username: username,
+        postId: postId,
+        datePublished: DateTime.now(),
+        postUrl: photoUrl,
+        profImage: profImage,
+        likes: [],
+      );
+
+      _fireStore.collection('posts').doc(postId).set(
+            post.toJson(),
+          );
+      res = 'success';
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+
+  Future<void> likePost(String postId, String uid, List likes) async {
+    try {
+      if (likes.contains(uid)) {
+        await _fireStore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayRemove([uid]),
+        });
+      } else {
+        await _fireStore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayUnion([uid]),
+        });
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+    return;
+  }
+
+  Future<String> postComment(
+      String profilePic, String name, String uid, String text, String postId) async {
+    String res = 'Some error occurred.';
+    try {
+      if (text.isNotEmpty) {
+        String commentId = Uuid().v1();
+        _fireStore
+            .collection('posts')
+            .doc(postId)
+            .collection('comments')
+            .doc(commentId)
+            .set({
+          'profilePic': profilePic,
+          'name': name,
+          'uid': uid,
+          'text': text,
+          'commentId': commentId,
+          'datepublished': DateTime.now(),
+        });
+        res = 'success';
+      } else {
+        res = "Please enter text";
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}

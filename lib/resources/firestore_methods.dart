5cd0cb265a57386b649d24f008b2b992539510c3
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-31 18:59:04 +0900

    ユーザーのフォロー機能、フォロー解除機能を追加

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -102,4 +102,34 @@ class FirestoreMethods {
       print(err.toString());
     }
   }
+
+  Future<void> followUser(String uid, String followId) async {
+    try {
+      DocumentSnapshot snap =
+          await _fireStore.collection('users').doc(uid).get();
+      List following = (snap.data()! as dynamic)['following'];
+
+      if (following.contains(followId)) {
+        await _fireStore.collection('users').doc(followId).update({
+          'followers': FieldValue.arrayRemove([uid])
+        });
+
+        await _fireStore.collection('users').doc(uid).update({
+          'following': FieldValue.arrayRemove([followId])
+        });
+      } else {
+        await _fireStore.collection('users').doc(followId).update({
+          'followers': FieldValue.arrayUnion([uid])
+        });
+
+        await _fireStore.collection('users').doc(uid).update({
+          'following': FieldValue.arrayUnion([followId])
+        });
+      }
+    } catch (err) {
+      print(
+        err.toString(),
+      );
+    }
+  }
 }
